1b9ff33d6d14997b53639b60d6ce0590bd226f1a
Danika <danikachrunik@gmail.com>
2017-11-19 19:53:26 -0800

    Step 3 to 5
    added steps 3 to 5

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,12 @@ $(function() {
    dataType: 'jsonp',
    success: function(response) {
      for(let course of response.courses.completed) {
-       $.("#badges").append("<div class='course'></div>");
+       console.log(course);
+       $( "#badges" ).append(
+        "<div class='course'><h3>" +
+        course.title + "</h3><img src='" +
+        course.badge + "'><a href='" +
+        course.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>" );
      }
    }
  });
